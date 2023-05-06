97e7af197d7961057865e4780cf3d81eaa039527
susum <susumeyyea@gmail.com>
2023-05-06 14:43:56 +0300

    initial data

---
+++ README.md
@@ -0,0 +1,1 @@
+# ds_movie_proj
---
+++ movie_data.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  6 14:34:20 2023
+
+@author: susum
+"""
+
+import pandas as pd
+import numpy as np
+
+df= pd.read_csv('C:/Users/susum/Documents/ds_movie_proj/Highest Holywood Grossing Movies.csv')
